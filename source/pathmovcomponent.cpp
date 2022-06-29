e04ca0bfd5fed947a8ed3fbc00ffdc2d827467c7
Andrea Attardo <105414820+andrea-attardo@users.noreply.github.com>
2022-06-29 16:21:06 +0200

    pathmovemente done but it need minor fixs.

--- source/pathmovcomponent.cpp
+++ source/pathmovcomponent.cpp
@@ -23,34 +23,45 @@ PathMovComponenet::PathMovComponenet( Actor* actor, json pathDescr ) {
     nextlegY    = path[nextleg][1];
     nextlegTime = path[nextleg][2];
 
-   
-    vx          = nextlegY - currentlegY / nextlegTime;
-    vy          = nextlegY - currentlegY / nextlegTime;
+    vx          = ( nextlegX - currentlegX ) / nextlegTime;
+    vy          = ( nextlegY - currentlegY ) / nextlegTime;
 
 }
 
 
 void PathMovComponenet::update( const double deltatime ) {
    
+    int distanceXs = nextlegX - pActor->getX();
+    int distanceYs = nextlegY - pActor->getY();
+
+    std::cout << distanceXs << " | " << distanceYs << "\n";
+
     //da sistemare  
-    if ( (int)pActor->getX() != (int)nextlegX && (int)pActor->getY() != (int)nextlegY )
+    //funziona ma: 
+    //1: se sposto la finestra quando sta vicino ai bordi se ne va
+    //2: se cambio la posizione iniziale della x e della y se ne va 
+    if ( distanceXs != 0 || distanceYs != 0 ) // <- problema con questa cosa qua
     {
         pActor->setX( pActor->getX() + ( vx * deltatime ) );
-        pActor->setY( pActor->getX() + ( vy * deltatime ) );
-    }
+        pActor->setY( pActor->getY() + ( vy * deltatime ) );
+        
+    } 
     else
     {
         currentleg = nextleg;
-        nextleg = ++nextleg % path.size();
+        currentlegX = path[currentleg][0];
+        currentlegY = path[currentleg][1];
 
+        nextleg = ++currentleg % path.size();
         nextlegX = path[nextleg][0];
         nextlegY = path[nextleg][1];
         nextlegTime = path[nextleg][2];
-        vx = nextlegX - currentlegX / nextlegTime;
-        vy = nextlegY - currentlegY / nextlegTime;
+
+        vx = ( nextlegX - currentlegX ) / nextlegTime;
+        vy = ( nextlegY - currentlegY ) / nextlegTime;
 
     }
-  
+    
     
     
 }
